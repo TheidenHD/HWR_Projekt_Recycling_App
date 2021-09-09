18a79b9893b3805b491668aa5be46bb511bb4b3e
TheidenHD <31984391+TheidenHD@users.noreply.github.com>
2021-09-09 08:25:36 +0200

    Initial commit

---
+++ app/src/main/java/com/theidenhd/hwr_projekt_recycling_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.theidenhd.hwr_projekt_recycling_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
