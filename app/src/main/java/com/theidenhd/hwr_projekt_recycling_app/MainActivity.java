21a1db5ec8e78dde0ba834777037b7b66f834437
TheidenHD <31984391+TheidenHD@users.noreply.github.com>
2021-09-28 15:19:06 +0200

    Hide Titel-Bar in Main

--- app/src/main/java/com/theidenhd/hwr_projekt_recycling_app/MainActivity.java
+++ app/src/main/java/com/theidenhd/hwr_projekt_recycling_app/MainActivity.java
@@ -2,8 +2,9 @@ package com.theidenhd.hwr_projekt_recycling_app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
 
         final Button button_scanner = findViewById(R.id.button);
